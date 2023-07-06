88b8e1905379758d4a6bc560592952dd0eb7b74e
fuad <aliyevfuad.989@gmail.com>
2023-07-06 14:24:11 +0400

    add BFS search

---
+++ src/main/java/org/code/data_structures/tree/binary_tree/BinarySearchTreeImpl.java
@@ -0,0 +1,137 @@
+package org.code.data_structures.tree.binary_tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinarySearchTreeImpl implements BinarySearchTree {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    BinarySearchTreeImpl() {
+        root = null;
+    }
+
+    //delete a node from BST
+    @Override
+    public void deleteKey(int key) {
+        root = deleteRecursive(root, key);
+    }
+
+    private Node deleteRecursive(Node root, int key) {
+        if (root == null) return root;
+        if (key < root.key)
+            root.left = deleteRecursive(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRecursive(root.right, key);
+        else {
+            //node contains only one child
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            // node has two children
+            //get inorder successor (min value in the right subtree)
+            root.key = minValue(root.right);
+            // Delete the inorder successor
+            root.right = deleteRecursive(root.right, root.key);
+        }
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minVal = root.key;
+        while (root.left != null) {
+            minVal = root.left.key;
+            root = root.left;
+        }
+        return minVal;
+    }
+
+    @Override
+    public void insert(int key) {
+        root = insertRecursive(root, key);
+    }
+
+    private Node insertRecursive(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)
+            root.left = insertRecursive(root.left, key);
+        else if (key > root.key)
+            root.right = insertRecursive(root.right, key);
+        return root;
+    }
+
+    @Override
+    public void inorder() {
+        inorderRecursive(root);
+    }
+
+    private void inorderRecursive(Node root) {
+        if (root != null) {
+            inorderRecursive(root.left);
+            System.out.println(root.key + " ");
+            inorderRecursive(root.right);
+        }
+    }
+
+    @Override
+    public boolean search(int key) {
+        root = searchRecursive(root, key);
+        if (root != null)
+            return true;
+        else
+            return false;
+    }
+
+    private Node searchRecursive(Node root, int key) {
+        if (root == null || root.key == key)
+            return root;
+        if (root.key > key)
+            return searchRecursive(root.left, key);
+        return searchRecursive(root.right, key);
+    }
+
+    @Override
+    public List<Integer> breadthFirstSearch() {
+        Node currentNode = this.root;
+        List<Integer> list = new ArrayList<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(currentNode);
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+            System.out.println(currentNode.key);
+            list.add(currentNode.key);
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        System.out.println(list);
+        return list;
+    }
+
+    @Override
+    public List<Integer> breadthFirstSearchRecursive(Queue<Node> queue, List<Integer> list) {
+        if (queue.size() == 0) return list;
+        Node currentNode = queue.remove();
+        list.add(currentNode.key);
+        if (currentNode.left != null) {
+            queue.add(currentNode.left);
+        }
+        if (currentNode.right != null) {
+            queue.add(currentNode.right);
+        }
+        return breadthFirstSearchRecursive(queue, list);
+    }
+}
--- src/main/java/org/code/data_structures/tree/binary_tree/Main.java
+++ src/main/java/org/code/data_structures/tree/binary_tree/Main.java
@@ -1,8 +1,13 @@
 package org.code.data_structures.tree.binary_tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
+        BinarySearchTreeImpl bst = new BinarySearchTreeImpl();
 
         /* BST tree example
               45
@@ -19,29 +24,37 @@ public class Main {
         bst.insert(90);
         bst.insert(50);
 
+        //breadth first search
+        System.out.println("BFS Search");
+//        bst.breadthFirstSearch();
+        Queue<Node> queue = new ArrayDeque();
+        queue.add(bst.getRoot());
+        List<Integer> values = bst.breadthFirstSearchRecursive(queue, new ArrayList<>());
+        System.out.println(values);
+
         //print the BST
-        System.out.println("The BST Created with input data(Left-root-right):");
-        bst.inorder();
-
-        //delete leaf node
-        System.out.println("\nThe BST after Delete 12(leaf node):");
-        bst.deleteKey(12);
-        bst.inorder();
-
-        //delete the node with one child
-        System.out.println("\nThe BST after Delete 90 (node with 1 child):");
-        bst.deleteKey(90);
-        bst.inorder();
-
-        //delete node with two children
-        System.out.println("\nThe BST after Delete 45 (Node with two children):");
-        bst.deleteKey(45);
-        bst.inorder();
-
-        //search a key in the BST
-        boolean ret_val = bst.search (50);
-        System.out.println("\nKey 50 found in BST:" + ret_val );
-        ret_val = bst.search (12);
-        System.out.println("\nKey 12 found in BST:" + ret_val );
+//        System.out.println("The BST Created with input data(Left-root-right):");
+//        bst.inorder();
+//
+//        //delete leaf node
+//        System.out.println("\nThe BST after Delete 12(leaf node):");
+//        bst.deleteKey(12);
+//        bst.inorder();
+//
+//        //delete the node with one child
+//        System.out.println("\nThe BST after Delete 90 (node with 1 child):");
+//        bst.deleteKey(90);
+//        bst.inorder();
+//
+//        //delete node with two children
+//        System.out.println("\nThe BST after Delete 45 (Node with two children):");
+//        bst.deleteKey(45);
+//        bst.inorder();
+//
+//        //search a key in the BST
+//        boolean ret_val = bst.search (50);
+//        System.out.println("\nKey 50 found in BST:" + ret_val );
+//        ret_val = bst.search (12);
+//        System.out.println("\nKey 12 found in BST:" + ret_val );
     }
 }
